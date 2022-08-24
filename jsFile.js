8a5295b23b284161e5cf058353b75e918232087b
hanneselfving <59114096+hanneselfving@users.noreply.github.com>
2022-08-24 09:56:02 +0200

    new branch

--- jsFile.js
+++ jsFile.js
@@ -9,52 +9,10 @@
      
    items[0].onclick = function() {
     modal.style.display = "block";
-    modal.getElementsByTagName('h2')[0].innerHTML = "Pathfinder"
-    modal.getElementsByTagName('img')[0].src = "greedysearch.gif"
-    modal.getElementsByClassName("modal-body")[0].innerHTML = "An application for demonstrating and testing various pathfinding algorithms"
+    modal.getElementsByTagName('h2')[0].innerHTML = "Ordtestet"
+    modal.getElementsByTagName('img')[0].src = "logo512x512svartbg.png"
+    modal.getElementsByClassName("modal-body")[0].innerHTML = "Ordtestet - bemästra svenska språket genom frågor & svar!"
     }
-
-    items[1].onclick = function() {
-        modal.style.display = "block";
-        modal.getElementsByTagName('h2')[0].innerHTML = "Sorting Algorithm Visualizer"
-        modal.getElementsByTagName('img')[0].src = "sorting visualizer screenshot.png"
-        modal.getElementsByClassName("modal-body")[0].innerHTML = "An application for demonstrating sorting algorithms"
-        }
-
-   items[2].onclick = function() {
-    modal.style.display = "block";
-    modal.getElementsByTagName('h2')[0].innerHTML = "Linux System Manager"
-    modal.getElementsByTagName('img')[0].src = "System Manager Screenshot.png"
-    modal.getElementsByClassName("modal-body")[0].innerHTML = "A shell script for convenient execution of sets of important UNIX/Linux commands"
-    }
-
-    items[3].onclick = function() {
-      modal.style.display = "block";
-      modal.getElementsByTagName('h2')[0].innerHTML = "Binary Search Tree Visualizer"
-      modal.getElementsByTagName('img')[0].src = "bst.PNG"
-      modal.getElementsByClassName("modal-body")[0].innerHTML = "An application for demonstrating a binary search tree and its operations"
-      }
-
-      items[4].onclick = function() {
-        modal.style.display = "block";
-        modal.getElementsByTagName('h2')[0].innerHTML = "Reliable Data Transfer Visualizer"
-        modal.getElementsByTagName('img')[0].src = "Reliable Data Transfer.gif"
-        modal.getElementsByClassName("modal-body")[0].innerHTML = "An application for demonstrating reliable data transfer"
-        }
-
-        items[5].onclick = function() {
-          modal.style.display = "block";
-          modal.getElementsByTagName('h2')[0].innerHTML = "Reliable Data Transfer Simulation"
-          modal.getElementsByTagName('img')[0].src = "setup-final v2.png"
-          modal.getElementsByClassName("modal-body")[0].innerHTML = "An application for simulating reliable data transfer"
-          }
-
-          items[6].onclick = function() {
-            modal.style.display = "block";
-            modal.getElementsByTagName('h2')[0].innerHTML = "Tower Defense"
-            modal.getElementsByTagName('img')[0].src = "screenshot.PNG"
-            modal.getElementsByClassName("modal-body")[0].innerHTML = "A tower defense game developed with Java Swing"
-            }
     
 
 

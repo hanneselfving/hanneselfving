5a95e9354f32541195a53164ad860f02f0c13bbe
hanneselfving <59114096+hanneselfving@users.noreply.github.com>
2022-12-14 21:31:50 +0100

    math tester box added

--- jsFile.js
+++ jsFile.js
@@ -15,56 +15,63 @@
                 modal.getElementsByClassName("modal-body")[0].innerHTML = "<p>Ordtestet - Practice your knowledge of the swedish language!</p><a href='https://play.google.com/store/apps/details?id=com.victoria.ordtestet&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>"
                 }
 
-   items[1].onclick = function() {
+    items[1].onclick = function() {
+                  modal.style.display = "block";
+                  modal.getElementsByTagName('h2')[0].innerHTML = "Math Tester"
+                  modal.getElementsByTagName('img')[0].style.visibility = 'hidden'
+                  modal.getElementsByClassName("modal-body")[0].innerHTML = "<p>Math Tester - Android App for getting good at math!</p><a href='https://play.google.com/store/apps/details?id=com.victoria.mathquiz_tracerbullet&gl=SE&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>"
+                  }            
+
+   items[2].onclick = function() {
     modal.style.display = "block";
     modal.getElementsByTagName('h2')[0].innerHTML = "Pathfinder"
     modal.getElementsByTagName('img')[0].src = "greedysearch.gif"
     modal.getElementsByClassName("modal-body")[0].innerHTML = "An application for demonstrating and testing various pathfinding algorithms<p><a href='https://github.com/hanneselfving/pathfindingalgorithms'>Link</a></p>"
     }
 
-    items[2].onclick = function() {
+    items[3].onclick = function() {
         modal.style.display = "block";
         modal.getElementsByTagName('h2')[0].innerHTML = "Sorting Algorithm Visualizer"
         modal.getElementsByTagName('img')[0].src = "sorting visualizer screenshot.png"
         modal.getElementsByClassName("modal-body")[0].innerHTML = "An application for demonstrating sorting algorithms<p><a href='https://github.com/hanneselfving/Sorting-Visualizer'>Link</a></p>"
         }
 
-   items[3].onclick = function() {
+   items[4].onclick = function() {
     modal.style.display = "block";
     modal.getElementsByTagName('h2')[0].innerHTML = "Linux System Manager"
     modal.getElementsByTagName('img')[0].src = "System Manager Screenshot.png"
     modal.getElementsByClassName("modal-body")[0].innerHTML = "A shell script for convenient execution of sets of important UNIX/Linux commands<p><a href='https://github.com/hanneselfving/Linux-System-Manager'>Link</a></p>"
     }
 
-    items[4].onclick = function() {
+    items[5].onclick = function() {
       modal.style.display = "block";
       modal.getElementsByTagName('h2')[0].innerHTML = "Binary Search Tree Visualizer"
       modal.getElementsByTagName('img')[0].src = "binarysearchtree.png"
       modal.getElementsByClassName("modal-body")[0].innerHTML = "An application for demonstrating a binary search tree and its operations<p><a href='https://github.com/hanneselfving/Binary-Search-Tree-Visualizer'>Link</a></p>"
       }
 
-      items[5].onclick = function() {
+      items[6].onclick = function() {
         modal.style.display = "block"; 
         modal.getElementsByTagName('h2')[0].innerHTML = "Reliable Data Transfer Visualizer"
         modal.getElementsByTagName('img')[0].src = "Reliable Data Transfer.gif"
         modal.getElementsByClassName("modal-body")[0].innerHTML = "An application for demonstrating reliable data transfer<p><a href='https://github.com/hanneselfving/RDP-Visualizer'>Link</a></p>"
         }
 
-        items[6].onclick = function() {
+        items[7].onclick = function() {
           modal.style.display = "block";
           modal.getElementsByTagName('h2')[0].innerHTML = "Reliable Data Transfer Simulation"
           modal.getElementsByTagName('img')[0].src = "setup-final v2.png"
           modal.getElementsByClassName("modal-body")[0].innerHTML = "An application for simulating reliable data transfer<p><a href='https://github.com/hanneselfving/Reliable-Data-Transfer-C'>Link</a></p>"
           }
 
-          items[7].onclick = function() {
+          items[8].onclick = function() {
             modal.style.display = "block";
             modal.getElementsByTagName('h2')[0].innerHTML = "Tower Defense"
             modal.getElementsByTagName('img')[0].src = "screenshot.PNG"
             modal.getElementsByClassName("modal-body")[0].innerHTML = "A tower defense game developed with Java Swing<p><a href='https://github.com/hanneselfving/Troll-Attack-TowerDefense'>Link</a></p>"
             }
 
-            items[8].onclick = function() {
+            items[9].onclick = function() {
               modal.style.display = "block";
               modal.getElementsByTagName('h2')[0].innerHTML = "Library Management System Design"
               modal.getElementsByTagName('img')[0].src = "Class Diagram of Library Management System.png"
